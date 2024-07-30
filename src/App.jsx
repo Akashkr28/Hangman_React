4a7981432fcbb6c9bcae158414e94886e38a4e98
Akashkr28 <email.akash2010@gmail.com>
2024-07-31 05:00:33 +0530

    code changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import Button from "./components/Button/Button"
+import TextInputForm from "./components/TextInputForm/TextInputForm";
 
 function App() {
   return (
     <div>
-      <h1 className="font-semibold text-3xl">Hello World!</h1>
-      <Button 
-        text="Secondary" 
-        buttonType="submit" 
-        styleType="secondary" 
-        onClickHandler={() => console.log("secondary clicked")}
-      />
-
-      <Button
-       text="Primary" 
-       buttonType="button" 
-       styleType="primary" 
-       onClickHandler={() => console.log("primary clicked")}
-      />
-
-      <Button 
-        text="Warning" 
-        buttonType="button" 
-        styleType="warning" 
-        onClickHandler={() => console.log("warning clicked")}
-      />
-
-<Button 
-        text="Error" 
-        buttonType="button" 
-        styleType="error" 
-        onClickHandler={() => console.log("error clicked")}
-      />
+      <h1 className="font-semibold text-3xl">Welcome to HangMan</h1>
+      <TextInputForm/>
     </div>
   );
 }
