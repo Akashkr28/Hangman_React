436f60e4dca1bc9f7c739cbf8eb4c6e3257435cd
Akashkr28 <email.akash2010@gmail.com>
2024-08-10 05:39:15 +0530

    added maskedText

---
+++ src/components/MaskedText/MaskedTextUtility.jsx
@@ -0,0 +1,12 @@
+export function getAllCharacters(word, usedLetters){
+    //This function will return a string with all the characters of the word that has been guessed so far.
+    usedLetters = usedLetters.map(letter => letter.toUpperCase()); // ['b', 'e'] -> ['B', 'E']
+    const guessedLetters = new Set(usedLetters); // {'B', 'E'}
+    const characters = word.toUpperCase().split('').map(char => {
+        if(guessedLetters.has(char)) {
+            return char;
+        }
+        return '_';
+    });
+    return characters.join(''); // ___B_E
+}
