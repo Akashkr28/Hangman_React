ea83faf550e5dafd7b353307eaaca53e8ded0abf
Akashkr28 <email.akash2010@gmail.com>
2024-08-11 06:27:01 +0530

    hangman added

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -2,15 +2,25 @@ import { Link, useLocation } from "react-router-dom";
 import MaskedText from "../../components/MaskedText/MaskedText";
 import LetterButtons from "../../components/LetterButtons/LetterButtons";
 import { useState } from "react";
+import HangMan from "../../components/HangMan/hangMan";
+
 
 function PlayGame() {
 
     const [usedLetters, setUsedLetters] = useState([]);
 
+    const [step,setStep] = useState(0);
+
     const location = useLocation();
     const wordsSelected = location.state?.wordsSelected; //If the state is not defined, it will return undefined, otherwise it will return the value of wordSelected
 
     const handleLetterClick = function(letter) {
+        if(wordsSelected.toUpperCase().includes(letter)){
+            console.log('Correct')
+        } else {
+            console.log('Incorrect')
+            setStep(step + 1);
+        }
         setUsedLetters([...usedLetters, letter]);
     }
 
@@ -19,12 +29,19 @@ function PlayGame() {
             <h1>Play Game</h1>
             <MaskedText text={wordsSelected} usedLetters={usedLetters}/>
             <hr/>
+            <div className="flex justify-between items-center">
+                <div className="basis-2/4">
+                    <LetterButtons text={wordsSelected} usedLetters={usedLetters} onLetterClick={handleLetterClick}/>
+                </div>
+                <div className="basis-2/4 flex justify-center items-center">
+                   <HangMan step={step}/>
+                </div>
+            </div>
 
-            <LetterButtons text={wordsSelected} usedLetters={usedLetters} onLetterClick={handleLetterClick}/>
             <hr/>
 
             <Link to='/start' className="text-blue-600 underline hover:text-blue-700">Start Game</Link>
         </div>
-    )
+    );
 }
 export default PlayGame;
